d10a0bb4b3310408a9385b5f5559f923503959da
Vadzim Simanau <eks220885@gmail.com>
2025-07-06 14:55:53 +0300

    replace SearchBar files in another folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import { Toaster } from "react-hot-toast";
+import SearchBar from "../src/components/SearchBar/SearchBar";
+import "./App.css";
+// Пакуль пакідаем іншыя кампаненты закаментаванымі, бо яны не патрэбны для гэтага кроку
+// import CafeInfo from '../CafeInfo/CafeInfo';
+// import VoteOptions from '../VoteOptions/VoteOptions';
+// import VoteStats from '../VoteStats/VoteStats';
+// import Notification from '../Notification/Notification';
+// import type { Votes, VoteType } from './types/votes';
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const [movies, setMovies] = useState([]);
+  const [query, setQuery] = useState("");
 
+  const handleSearchSubmit = (searchQuery: string) => {
+    console.log("Search query from SearchBar:", searchQuery);
+    setQuery(searchQuery);
+  };
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="app">
+      <Toaster /> {}
+      <SearchBar onSubmit={handleSearchSubmit} />
+      {/* MovieGrid, Loader, ErrorMessage */}
+      {/* <CafeInfo /> */}
+      {/* <VoteOptions ... /> */}
+      {/* <VoteStats ... /> */}
+      {/* <Notification /> */}
+    </div>
+  );
 }
-
-export default App
