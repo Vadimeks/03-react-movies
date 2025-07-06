506e596c3d8461b4ac5d8dd95f8b10ca01ac8dac
Vadzim Simanau <eks220885@gmail.com>
2025-07-06 15:25:43 +0300

    add movieGrid component

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,39 @@
 import { useState } from "react";
 import { Toaster } from "react-hot-toast";
-import SearchBar from "../src/components/SearchBar/SearchBar";
 import "./App.css";
+import SearchBar from "../src/components/SearchBar/SearchBar";
+import MovieGrid from "../src/components/MovieGrid/MovieGrid";
+import type { Movie } from "./types/movie";
 // Пакуль пакідаем іншыя кампаненты закаментаванымі, бо яны не патрэбны для гэтага кроку
 // import CafeInfo from '../CafeInfo/CafeInfo';
 // import VoteOptions from '../VoteOptions/VoteOptions';
 // import VoteStats from '../VoteStats/VoteStats';
 // import Notification from '../Notification/Notification';
-// import type { Votes, VoteType } from './types/votes';
+//
 
 export default function App() {
-  const [movies, setMovies] = useState([]);
+  const [movies, setMovies] = useState<Movie[]>([]);
   const [query, setQuery] = useState("");
 
   const handleSearchSubmit = (searchQuery: string) => {
     console.log("Search query from SearchBar:", searchQuery);
     setQuery(searchQuery);
+    setMovies([]);
   };
+
+  const handleMovieSelect = (movie: Movie) => {
+    console.log("Selected movie:", movie);
+  };
+
   return (
     <div className="app">
-      <Toaster /> {}
+      <Toaster />
       <SearchBar onSubmit={handleSearchSubmit} />
-      {/* MovieGrid, Loader, ErrorMessage */}
-      {/* <CafeInfo /> */}
-      {/* <VoteOptions ... /> */}
-      {/* <VoteStats ... /> */}
-      {/* <Notification /> */}
+      {/* Рэндэрым MovieGrid, калі ёсць фільмы */}
+      {movies.length > 0 && (
+        <MovieGrid movies={movies} onSelect={handleMovieSelect} />
+      )}
+      {/* Тут будуць Loader і ErrorMessage у наступных кроках */}
     </div>
   );
 }
